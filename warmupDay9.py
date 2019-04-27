6fed935c88171842caa66e741ba1ac3986db4ace
lindseyc03 <lindsey.cr123@gmail.com>
2019-04-27 15:24:43 +0000

    warmup

--- warmupDay9.py
+++ warmupDay9.py
@@ -1,1 +1,25 @@
 # File name: warmupDay9.py
+count = 1
+for i in range(1,5):
+  count *= i 
+  print(count)
+
+largestnum = 0 
+for i in range(10):
+  if i > largestnum:
+    largestnum = i
+  print(largestnum)
+
+userinput = input('Enter a string:')
+inputlength = len(userinput)
+print(inputlength-1)
+
+foodlist = ['milk', 'juice', 'cookies']
+listlength = len(foodlist)
+if listlength == 0:
+  print('Your list is empty')
+else:
+  print('Your list is not empty')
+
+listone = [1, 3, 5, 7]
+listtwo = [1, 2, 4, 6]
